d0ed3c9d5c8890a526cbdde831f6fc03f391db0d
linshuguang <linshuguang@xiaomi.com>
2019-01-26 17:54:52 +0800

    first commit

---
+++ demo/src/main/java/com/pcube/provider/DemoServiceImpl.java
@@ -0,0 +1,39 @@
+package com.pcube.provider;
+
+import com.pcube.service.provider.demo.DemoService;
+import com.pcube.service.provider.demo.TPDemo;
+import com.pcube.service.provider.demo.TRDemo;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * Created by kenya on 2019/1/26.
+ */
+public class DemoServiceImpl implements DemoService{
+    private static final Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
+
+    public TRDemo hello(TPDemo tpDemo){
+        logger.info("param:{}", tpDemo);
+        TRDemo trDemo = new TRDemo();
+        trDemo.setDemo("hello "+tpDemo.getDemo());
+        logger.info("response:{}", trDemo);
+        return trDemo;
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"application.xml"});
+        context.start();
+        logger.info("提供者服务已注册成功");
+        try {
+            System.in.read();//让此程序一直跑，表示一直提供服务
+        } catch (IOException e) {
+            logger.error(ExceptionUtils.getStackTrace(e));
+        }
+    }
+}
+
